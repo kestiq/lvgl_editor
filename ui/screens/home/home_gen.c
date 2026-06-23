a0dc9bde645cdd389112352aaf5a1ce1a7aac624
kestiq <konstantinkengie@gmail.com>
2026-06-24 01:12:03 +0300

    1

---
+++ ui/screens/home/home_gen.c
@@ -0,0 +1,56 @@
+/**
+ * @file home_gen.c
+ * @brief Template source file for LVGL objects
+ */
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+#include "home_gen.h"
+#include "../../ui.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/***********************
+ *  STATIC VARIABLES
+ **********************/
+
+/***********************
+ *  STATIC PROTOTYPES
+ **********************/
+
+/**********************
+ *   GLOBAL FUNCTIONS
+ **********************/
+
+lv_obj_t * home_create(void)
+{
+    LV_TRACE_OBJ_CREATE("begin");
+
+
+    static bool style_inited = false;
+
+    if (!style_inited) {
+
+        style_inited = true;
+    }
+
+    lv_obj_t * lv_obj_0 = lv_obj_create(NULL);
+    lv_obj_set_name_static(lv_obj_0, "home_#");
+
+    LV_TRACE_OBJ_CREATE("finished");
+
+    return lv_obj_0;
+}
+
+/**********************
+ *   STATIC FUNCTIONS
+ **********************/
+
